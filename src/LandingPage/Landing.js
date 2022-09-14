e5af410c0b9eac2c35bb5615a206b6e0595442d5
Dat <todat2001@gmail.com>
2022-09-14 13:14:20 +0700

    add search part

--- src/LandingPage/Landing.js
+++ src/LandingPage/Landing.js
@@ -1,7 +1,15 @@
+import React from "react";
 import Header from "./Header/Header";
+import Search from "./Search/Search";
+
 import classes from "./Landing.module.css";
 
 const Landing = (props) => {
-  return <Header></Header>;
+  return (
+    <>
+      <Header></Header>
+      <Search />
+    </>
+  );
 };
 export default Landing;
---
+++ src/LandingPage/Search/Search.js
@@ -0,0 +1,17 @@
+import Button from "../../Layout/Button/Button";
+import classes from "./Search.module.css";
+
+const Search = (props) => {
+  return (
+    <div className={classes.container}>
+      <form className={classes.form}>
+        <input
+          className={classes["form-input"]}
+          placeholder="Tìm căn hộ tại đây..."
+        />
+        <Button className={classes["form-button"]}>Tìm kiếm</Button>
+      </form>
+    </div>
+  );
+};
+export default Search;
