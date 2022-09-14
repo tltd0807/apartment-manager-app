7170047ed8881859bdfe3d76ddea69313c10ed2b
Dat <todat2001@gmail.com>
2022-09-14 12:44:45 +0700

    add heading

---
+++ src/LandingPage/Header/Header.js
@@ -0,0 +1,32 @@
+import classes from "./Header.module.css";
+
+import Button from "../../Layout/Button/Button";
+import IMG from "../../Image/illustration-working.svg";
+const Header = (props) => {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <div className={classes.name}>MyHome</div>
+      </nav>
+      <div className={classes.container}>
+        <div className={classes["header-content"]}>
+          <div className={classes.left}>
+            <h1>Chọn mái ấm tốt nhất cho gia đình bạn</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Iaculis nunc sed augue lacus viverra vitae congue.
+            </p>
+            <div className={classes.login}>
+              <Button>Đăng nhập</Button>
+            </div>
+          </div>
+          <div>
+            <img src={IMG} className={classes["content-img"]} />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
---
+++ src/LandingPage/Landing.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header";
+import classes from "./Landing.module.css";
+
+const Landing = (props) => {
+  return <Header></Header>;
+};
+export default Landing;
