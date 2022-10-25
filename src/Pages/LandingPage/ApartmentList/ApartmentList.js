3fe0bb71fde59166d1f0c4971b49c6295e9309c5
Dat <todat2001@gmail.com>
2022-10-25 22:07:22 +0700

    change search method

--- src/Pages/LandingPage/ApartmentList/ApartmentList.js
+++ src/Pages/LandingPage/ApartmentList/ApartmentList.js
@@ -9,6 +9,7 @@ import classes from "./ApartmentList.module.css";
 const ApartmentList = (props) => {
   const [apartmentArr, setApartmentArr] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [isAll, setIsAll] = useState(true);
 
   // ngăn cho getData re-create khi ApartmentList re-eveluating
   const getData = useCallback(async (pageNumber) => {
@@ -16,12 +17,9 @@ const ApartmentList = (props) => {
       const res = await axios.get(
         `https://buildingmanager-api.herokuapp.com/api/System/items?PageNumber=${pageNumber}`
       );
-      // "https://buildingmanager-api.herokuapp.com/api/System/items?PageNumber=0"
-
-      // Sử dụng cái này khi mà cần update state dựa trên state cũ
 
       if (pageNumber === 1) {
-        setApartmentArr([...res["data"]["data"]]);
+        setApartmentArr(res["data"]["data"]);
       } else {
         // nếu pageNumber >0 thì xử lý cho button xem thêm
         setApartmentArr((apartmentArr) => [
@@ -34,13 +32,36 @@ const ApartmentList = (props) => {
     }
   }, []);
 
+  const getDataBtType = useCallback(async (typeId) => {
+    if (typeId === 0) {
+      // setIsAll(true);
+      setCurrentPage(1);
+      getData(1);
+    } else {
+      try {
+        const res = await axios.get(
+          `https://buildingmanager-api.herokuapp.com/api/System/items?Types=${typeId}`
+        );
+        setApartmentArr(res.data.data);
+      } catch (e) {
+        console.log("ERROR", e);
+      }
+    }
+  }, []);
+
   useEffect(() => {
-    getData(currentPage);
+    if (isAll) {
+      getData(currentPage);
+    } else {
+      // tạm thời k làm gì hết
+      // setIsAll(false);
+      return;
+    }
   }, [currentPage]);
 
   return (
     <>
-      <Search className={classes["half-down"]} />
+      <Search className={classes["half-down"]} changeType={getDataBtType} />
       <div className={classes["list-container"]}>
         {apartmentArr.length === 0 && (
           <p>Something went wrong. Please try it again</p>
--- src/Pages/LandingPage/Search/Search.js
+++ src/Pages/LandingPage/Search/Search.js
@@ -4,13 +4,47 @@ import classes from "./Search.module.css";
 const Search = (props) => {
   return (
     <div className={`${classes.container} ${props.className}`}>
-      <form className={classes.form}>
-        <input
+      <div className={classes.form}>
+        {/* <input
           className={classes["form-input"]}
           placeholder="Tìm căn hộ tại đây..."
-        />
-        <Button className={classes["form-button"]}>Tìm kiếm</Button>
-      </form>
+        /> */}
+        {/* không search */}
+        <Button
+          className={classes["form-button"]}
+          onClick={() => props.changeType(0)}
+        >
+          Tất Cả
+        </Button>
+        {/* search với id =1 */}
+        <Button
+          className={classes["form-button"]}
+          onClick={() => props.changeType(1)}
+        >
+          Studio
+        </Button>
+        {/* search với id =2 */}
+        <Button
+          className={classes["form-button"]}
+          onClick={() => props.changeType(2)}
+        >
+          2PN 1VS
+        </Button>
+        {/* search với id =3 */}
+        <Button
+          className={classes["form-button"]}
+          onClick={() => props.changeType(3)}
+        >
+          2PN 2VS
+        </Button>
+        {/* search với id =4 */}
+        <Button
+          className={classes["form-button"]}
+          onClick={() => props.changeType(4)}
+        >
+          3PN 2VS
+        </Button>
+      </div>
     </div>
   );
 };
