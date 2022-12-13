52f7a5f1d3788c1e10c5e009dd2659b43d2c7370
Dat <todat2001@gmail.com>
2022-12-14 02:49:22 +0700

    fix sigin bug

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,15 +1,19 @@
 import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Spin, Form, Input } from "antd";
+import { Button, Spin, Form, Input, message } from "antd";
 import AuthContext from "../../store/auth-context";
 import { loginUser } from "../../API/authAPI";
 import classes from "./SignIn.module.css";
 const SignIn = () => {
   const [loading, setLoading] = useState(false);
   const authCtx = useContext(AuthContext);
-  const navigate = useNavigate();
+  const [messageApi, contextHolder] = message.useMessage();
 
+  const navigate = useNavigate();
+  const error = (mes) => {
+    messageApi.error(mes);
+  };
   const onFinish = (values) => {
     // console.log("Received values of form: ", values);
     setLoading(true);
@@ -28,73 +32,78 @@ const SignIn = () => {
       })
       .catch((err) => {
         console.log(err);
+        error("Thất bại, vui lòng kiểm tra lại username và password");
+        setLoading(false);
       });
   };
   return (
-    <section
-      className={classes["signup"]}
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-      }}
-    >
-      {loading && <Spin />}
-      {!loading && (
-        <div className={classes.form}>
-          <Form
-            name="normal_login"
-            className="login-form"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={onFinish}
-          >
-            <Form.Item
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Username!",
-                },
-              ]}
+    <>
+      {contextHolder}
+      <section
+        className={classes["signup"]}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
+      >
+        {loading && <Spin />}
+        {!loading && (
+          <div className={classes.form}>
+            <Form
+              name="normal_login"
+              className="login-form"
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
             >
-              <Input
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="Username"
-              />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Password!",
-                },
-              ]}
-            >
-              <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Item>
-
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="login-form-button"
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Username!",
+                  },
+                ]}
               >
-                Đăng nhập
-              </Button>
-              <br /> Chưa có tài khoản?<Link to="/signup"> Đăng ký ngay</Link>
-            </Form.Item>
-          </Form>
-        </div>
-      )}
-    </section>
+                <Input
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="Username"
+                />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Password!",
+                  },
+                ]}
+              >
+                <Input
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >
+                  Đăng nhập
+                </Button>
+                <br /> Chưa có tài khoản?<Link to="/signup"> Đăng ký ngay</Link>
+              </Form.Item>
+            </Form>
+          </div>
+        )}
+      </section>
+    </>
   );
 };
 export default SignIn;
